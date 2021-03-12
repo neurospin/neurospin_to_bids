cec428c6de4f44a32f6d724beca23aeaef8fed1a
Yann Leprince <yann.leprince@cea.fr>
2021-03-12 16:23:38 +0100

    cleaner failure message for tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ import subprocess
 
 def test_app_help():
     exe = distutils.spawn.find_executable('neurospin_to_bids')
+    assert exe is not None
     print('Testing neurospin_to_bids executable: ' + exe)
     assert subprocess.call([exe, '-h']) == 0
     assert subprocess.call([exe, '--help']) == 0
