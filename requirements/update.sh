14c458f9bff722ef51bbd1b6a9d7602ed4a54b53
Yann Leprince <yann.leprince@cea.fr>
2025-03-05 16:22:10 +0100

    Use a different set of pinned package versions for Python 3.12+

--- requirements/update.sh
+++ requirements/update.sh
@@ -6,11 +6,24 @@
 #     ./requirements/update.sh -U
 
 export CUSTOM_COMPILE_COMMAND=./requirements/update.sh
+
+if python -c 'import sys; sys.exit(0 if sys.version_info[:2] == (3, 12) else 1)'; then
+  PY_VER=py3.12
+elif python -c 'import sys; sys.exit(0 if sys.version_info[:2] == (3, 8) else 1)'; then
+  PY_VER=py3.8
+else
+  echo "This script must be run either on Python 3.8 (to generate the dependency" >&2
+  echo "pinnings for Ubuntu 20.04 and 22.04) or Python 3.12 (to generate the" >&2
+  echo "dependency pinnings for Ubuntu 24.04 or later" >&2
+  exit 1
+fi
+
 # --strip-extras is necessary because we use requirements/*.txt as PIP
 # --constraint files (-c option)
 python -m piptools compile \
        --allow-unsafe --strip-extras --resolver=backtracking \
-       --output-file=requirements/production.txt "$@"
+       --output-file=requirements/${PY_VER}-production.txt "$@"
 python -m piptools compile \
        --allow-unsafe --strip-extras --resolver=backtracking \
-       requirements/test.in "$@"
+       --output-file=requirements/${PY_VER}-test.txt \
+       requirements/${PY_VER}-test.in "$@"
